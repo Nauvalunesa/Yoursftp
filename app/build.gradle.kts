2c06842b76c9ca97dcf83b7656f534fb44441a81
Nauvalunesa <24031554092@mhs.unesa.ac.id>
2026-06-19 19:58:43 +0700

    Update APK version to 1.0.3 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yoursftp.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.3"
         vectorDrawables { useSupportLibrary = true }
     }
 
