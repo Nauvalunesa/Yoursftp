bb6874f30155cc16b7babc56a5250b11aaeff4c5
Nauvalunesa <24031554092@mhs.unesa.ac.id>
2026-07-13 23:39:58 +0700

    chore: bump version to 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yoursftp.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.8"
+        versionCode = 8
+        versionName = "1.0.9"
         vectorDrawables { useSupportLibrary = true }
     }
 
